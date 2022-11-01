01cbe8d91033161a33894d287e3a96b1b78fbea1
shloyem <39220943+Shloyem@users.noreply.github.com>
2022-11-01 12:05:50 -0400

    added tests that only owner can increase total supply

--- src/VolcanoCoin.sol
+++ src/VolcanoCoin.sol
@@ -24,7 +24,7 @@ contract VolcanoCoin is Ownable {
         return totalSupply;
     }
 
-    function increaseTotalSupply() public {
+    function increaseTotalSupply() public onlyOwner {
         totalSupply += 1000;
         emit SupplyChanged(totalSupply);
     }
--- test/VolcanoCoin.t.sol
+++ test/VolcanoCoin.t.sol
@@ -5,11 +5,25 @@ import "forge-std/Test.sol";
 import "../src/VolcanoCoin.sol";
 
 contract VolcanoCoinTest is Test {
-    VolcanoCoin public volcanoCoin;
-    uint256 public totalSupply;
+    VolcanoCoin private volcanoCoin;
+
+    function setUp() public {
+        volcanoCoin = new VolcanoCoin();
+    }
 
     function testInitialTotalSupply() public {
-        volcanoCoin = new VolcanoCoin();
         assertEq(volcanoCoin.totalSupply(), 10000);
+    }
+
+    function testTotalSupplyIncrease() public {
+        assertEq(volcanoCoin.totalSupply(), 10000);
+        volcanoCoin.increaseTotalSupply();
+        assertEq(volcanoCoin.totalSupply(), 10000 + 1000);
+    }
+
+    function testOnlyOwnerIncreases(uint256 x) public {
+        vm.expectRevert("Ownable: caller is not the owner");
+        vm.prank(address(0));
+        volcanoCoin.increaseTotalSupply();
     }
 }
